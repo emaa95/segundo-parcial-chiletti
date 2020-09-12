f573f2192b92cf43d95a4ca34d54730baba3e811
emaa95 <chilettiemanuel@gmail.com>
2020-09-12 19:05:58 -0300

    Modelo actividad creado y atributos actualizados

---
+++ api/models/Actividad.js
@@ -0,0 +1,35 @@
+/**
+ * Actividad.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    kilosUpdate: { type: 'number', required: true, },
+    tipo: { type: 'string', required: true,},
+    subtotal: {type: 'number', required: true,},
+    total: {type: 'number'},
+    comentario: { type: 'string'},
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    stock:{
+      model: 'stock'
+    }
+  },
+
+};
+
